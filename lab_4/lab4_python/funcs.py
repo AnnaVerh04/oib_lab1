767772ca660abc3c21e3360d0d1ecb130afdf197
AnyaVerkhovaia <anyaverkhovaia@gmail.com>
2024-05-30 10:27:51 +0400

    Adding a docstring

--- lab_4/lab4_python/funcs.py
+++ lab_4/lab4_python/funcs.py
@@ -1,11 +1,20 @@
 import hashlib
 import multiprocessing as mp
+import queue
+
 from matplotlib import pyplot as plt
 import tqdm
 import time
 
 
-def luna_alg_check_card_number(card_num: str):
+def luna_alg_check_card_number(card_num: str) -> bool:
+    """
+    Проверяет валидность номера банковской карты с помощью алгоритма Луна.
+        card_num (str): Номер банковской карты для проверки.
+        bool: True, если номер карты валиден, иначе False.
+    """
+    if not card_num.isdigit():
+        raise ValueError("Номер карты должен содержать только цифры.")
     card_num_arr = [int(a) for a in card_num]
     for i in range(len(card_num_arr) - 2, -1, -2):
         card_num_arr[i] *= 2
@@ -14,7 +23,16 @@ def luna_alg_check_card_number(card_num: str):
     return sum(card_num_arr) % 10 == 0
 
 
-def get_right_symbols(cur_hash, begins, end, range_b, range_e, q):
+def get_right_symbols(cur_hash: str, begins: list, end: str, range_b: int, range_e: int, q: queue) -> None:
+    """
+        Находит правильные символы для заданного хэша.
+            cur_hash (str): Хэш, который необходимо сопоставить.
+            begins (list): Список начальных символов.
+            end (str): Конечный символ.
+            range_b (int): Начало диапазона чисел.
+            range_e (int): Конец диапазона чисел.
+            q (Queue): Очередь для сохранения результатов.
+    """
     res = []
     for i in range(range_b, range_e):
         i = str(i).rjust(6, '0')
@@ -26,7 +44,15 @@ def get_right_symbols(cur_hash, begins, end, range_b, range_e, q):
     q.put(res)
 
 
-def find_right_numbers(cur_hash, begins, end, n_o_p):
+def find_right_numbers(cur_hash: str, begins: list, end: str, n_o_p: int) -> list:
+    """
+        Находит правильные числа для заданного хэша, используя несколько процессов.
+            cur_hash (str): Хэш, который необходимо сопоставить.
+            begins (list): Список начальных символов.
+            end (str): Конечный символ.
+            n_o_p (int): Количество процессов для параллельного выполнения.
+            list: Список правильных чисел для заданного хэша.
+    """
     process_arr = []
     queue_arr = []
     for i in range(n_o_p):
@@ -44,7 +70,15 @@ def find_right_numbers(cur_hash, begins, end, n_o_p):
     return res_total
 
 
-def get_dependency(cur_hash, begins, end, max_n_o_p=None):
+def get_dependency(cur_hash: str, begins: list, end: str, max_n_o_p: int = None) -> tuple:
+    """
+       Получает зависимость времени выполнения от количества процессов.
+           cur_hash (str): Хэш, который необходимо сопоставить.
+           begins (list): Список начальных символов.
+           end (str): Конечный символ.
+           max_n_o_p (int): Максимальное количество процессов. Если не указано, используется 1.5 * количество ядер.
+           tuple: Время выполнения для каждого количества процессов, результат для последнего количества процессов.
+    """
     time_arr = []
     if max_n_o_p is None:
         max_n_o_p = int(mp.cpu_count() * 1.5)
@@ -60,7 +94,16 @@ def get_dependency(cur_hash, begins, end, max_n_o_p=None):
     return time_arr, res2
 
 
-def get_plot(cur_hash, begins, end, max_n_o_p=None, save_img=True):
+def get_plot(cur_hash: str, begins: list, end: str, max_n_o_p: int = None, save_img: bool = True) -> list:
+    """
+        Получает график зависимости времени выполнения от количества процессов.
+            cur_hash (str): Хэш, который необходимо сопоставить.
+            begins (list): Список начальных символов.
+            end (str): Конечный символ.
+            max_n_o_p (int): Максимальное количество процессов. Если не указано, используется 1.5 * количество ядер.
+            save_img (bool): Флаг для сохранения изображения графика.
+            list: Результат выполнения функции get_dependency.
+    """
     if max_n_o_p is None:
         max_n_o_p = int(mp.cpu_count() * 1.5)
     time_arr, card_codes = get_dependency(cur_hash, begins, end, max_n_o_p)
